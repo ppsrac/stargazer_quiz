5a60b4381408d3b30fb8cc76a26626ac0a5861da
김수현 <ppsracchriskim@gmail.com>
2024-02-10 01:29:23 +0900

    [Feat] Add global variable star_loc_data, star_info_data and initialize it in __init__

--- flask_stargazer_quiz/app.py
+++ flask_stargazer_quiz/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import time
 from utils.load_utils.load_star_data import load_star_data, trans_loc_data
 from utils.time_utils.get_sidereal_time import get_sidereal_time
+
+# data file path
+data_path = 'data/hyg_v37.csv'
+
+star_loc_data = None
+star_info_data = None
 
 
 # Init method.
@@ -8,8 +15,10 @@ from utils.time_utils.get_sidereal_time import get_sidereal_time
 
 class StarGazerApp(Flask):
     def __init__(self, *args, **kwargs):
+        global star_loc_data, star_info_data
         super().__init__(*args, **kwargs)
-        print("Custom Hello")
+        loc_data, star_info_data = load_star_data(data_path)
+        star_loc_data = trans_loc_data(loc_data)
 
 
 app = StarGazerApp(__name__)
